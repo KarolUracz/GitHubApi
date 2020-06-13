f5a21594c7be8048a152d1adf2d51dfb09e994cc
Karol Uracz <karol_ur@interia.pl>
2020-06-13 10:42:59 +0200

    some tests

---
+++ src/test/java/uracz/karol/recruitmenttask/controller/ConnectionControllerTest.java
@@ -0,0 +1,52 @@
+package uracz.karol.recruitmenttask.controller;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import uracz.karol.recruitmenttask.service.GitHubApiService;
+
+import javax.servlet.ServletContext;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class ConnectionControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @MockBean
+    private GitHubApiService gitHubApiService;
+
+    @Test
+    public void givenWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = wac.getServletContext();
+
+        Assert.assertNotNull(servletContext);
+        Assert.assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(wac.getBean("connectionController"));
+    }
+
+    @Test
+    void shouldReturn() {
+    }
+}
---
+++ src/test/java/uracz/karol/recruitmenttask/service/GitHubApiServiceTest.java
@@ -0,0 +1,16 @@
+package uracz.karol.recruitmenttask.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GitHubApiServiceTest {
+
+    @Test
+    void getDataFromGitHubApi() {
+    }
+
+    @Test
+    void getDetailsDataFromUrl() {
+    }
+}
