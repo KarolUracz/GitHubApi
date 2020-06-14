7806ca84f1d33eb59a524e2b8710f2187e078411
Karol Uracz <karol_ur@interia.pl>
2020-06-14 11:10:12 +0200

    code refactor

--- src/main/java/uracz/karol/recruitmenttask/controller/ConnectionController.java
+++ src/main/java/uracz/karol/recruitmenttask/controller/ConnectionController.java
@@ -1,21 +1,14 @@
 package uracz.karol.recruitmenttask.controller;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import uracz.karol.recruitmenttask.service.GitHubApiService;
 
-import java.io.*;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @RestController
 public class ConnectionController {
